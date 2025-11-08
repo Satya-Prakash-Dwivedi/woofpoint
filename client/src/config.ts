31e3cbad4d9a434cad23234dfbb97f5e0b66c0ba
Satya-Prakash-Dwivedi <dwivediprakashsatya36@gmail.com>
2025-11-08 17:30:01 +0530

    Add docker setup and configuration

--- client/src/config.ts
+++ client/src/config.ts
@@ -2,7 +2,7 @@ const dev = {
     apiUrl : "http://localhost:3001/api",
 }
 const prod = {
-    apiUrl : "https://prod_url.com/api",
+    apiUrl : "https://9xhmunc89s.us-east-1.awsapprunner.com/api",
 }
 
 const config = process.env.NODE_ENV === 'development' ? dev : prod;
