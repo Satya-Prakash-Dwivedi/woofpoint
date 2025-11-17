71b8da5d9b6f539a00807c9a8e2c450a65e3ab75
Satya-Prakash-Dwivedi <dwivediprakashsatya36@gmail.com>
2025-11-17 08:49:07 +0530

    Add: 1. Separate env logic for dev and prod added

--- woofpoint-server/server.ts
+++ woofpoint-server/server.ts
@@ -4,9 +4,21 @@ import cors from "cors"
 import userRoutes from "./routes/user.routes"
 import trainerRoutes from "./routes/trainer.routes"
 import ownerRoutes from "./routes/owner.routes"
-
+import path from "path"
 import dotenv from "dotenv"
-dotenv.config()
+
+// Default to 'development' if NODE_ENV isn't set
+const nodeEnv = process.env.NODE_ENV || 'development';
+
+dotenv.config({
+  path: path.resolve(__dirname , `./.env.${nodeEnv}` )
+})
+
+// Check mongodb connection
+if (!process.env.MONGODB_URI) {
+  console.error("FATAL ERROR: ❌ MONGODB_URI is not defined.");
+  process.exit(1);
+}
 
 const PORT = process.env.PORT || 3001;
 const app = express()
@@ -15,7 +27,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/health", (_req, res) => {
-  res.status(200).json({ status: "UP" });
+  res.status(200).json({ status: "Server is UP now" });
 });
 
 app.use("/api/auth", userRoutes)
@@ -25,7 +37,7 @@ app.use("/api/owner", ownerRoutes);
 const startServer = async () => {
    try {
     await mongoose.connect(process.env.MONGODB_URI as string);
-    console.log("MongoDB Connection ✅");
+    console.log(`MongoDB Connection ✅ (Env: ${nodeEnv})`);
      app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
    } catch (err) {
      console.error("❌ Failed to start server:", err);
